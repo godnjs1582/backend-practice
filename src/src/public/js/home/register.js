51b7b92f9cb6d2214798be0c31fbb96a4c63a750
kwonhaewon <kwonhaewon@kwonhaewons-MacBook-Pro.local>
2022-10-12 17:53:30 +0900

    FEAT:회원가입 요청 구현

---
+++ src/src/public/js/home/register.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const 
+  id = document.querySelector("#id"),
+  name =document.querySelector("#name"),
+  password = document.querySelector("#password"),
+  confirmPassword = document.querySelector("#confirm-password"),
+  registerBtn = document.querySelector("#button");
+
+
+
+const register = () => {
+  const req = {
+    id: id.value,
+    name:name.value,
+    password: password.value,
+    confirmPassword:confirmPassword.value
+  };
+  console.log(req)
+
+  // fetch("/register", {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify(req),
+  // })
+  //   .then((res) => res.json())
+  //   .then((res) => {
+  //     if (res.success) {
+  //       location.href = "/login";
+  //     } else {
+  //       alert(res.msg);
+  //     }
+  //   })
+  //   .catch((err)=>console.error("회원가입 중 에러 발생"))
+    
+    
+};
+registerBtn.addEventListener("click", register);
+
